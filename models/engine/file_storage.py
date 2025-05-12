a77d1ff58410fd1ba99ffe4d466aad9ebf241111
Chalo1996 <emusyoka759@gmail.com>
2025-05-12 11:24:01 +0300

    Updates: Bind class attributes to class methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,41 +38,45 @@ class FileStorage:
     def __init__(self) -> None:
         """Instantiates the FileStorage class"""
         pass
-
-    def all(self):
+    
+    @classmethod
+    def all(cls):
         """returns the dictionary __objects
         """
-        return self.__objects
+        return cls.__objects
 
-    def new(self, obj):
+    @classmethod
+    def new(cls, obj):
         """sets in __objects the obj with key <obj class name>.id
         """
         obj_name = obj.__class__.__name__
         obj_id = obj.id
         if obj:
             obj_repr = f"{obj_name}.{obj_id}"
-            self.__objects[obj_repr] = obj
+            cls.__objects[obj_repr] = obj
 
-    def save(self):
+    @classmethod
+    def save(cls):
         """serializes __objects to the JSON file (path: __file_path)
         """
-        to_serialize = {k: v.to_dict() for k, v in self.__objects.items()}
+        to_serialize = {k: v.to_dict() for k, v in cls.__objects.items()}
 
-        with open(self.__file_path, "w", encoding="utf-8") as f:
+        with open(cls.__file_path, "w", encoding="utf-8") as f:
             json.dump(to_serialize, f, indent=4)
 
-    def reload(self):
+    @classmethod
+    def reload(cls):
         """deserializes the JSON file to __objects
         """
 
-        if os.path.exists(self.__file_path):
+        if os.path.exists(cls.__file_path):
             try:
-                with open(self.__file_path, "r", encoding="utf-8") as f:
+                with open(cls.__file_path, "r", encoding="utf-8") as f:
                     object_dict = json.load(f)
                     for key, val in object_dict.items():
                         obj_name = val["__class__"]
                         obj = classes[obj_name]
-                        self.__objects[key] = obj(**val)
+                        cls.__objects[key] = obj(**val)
 
             except FileNotFoundError:
                 pass
